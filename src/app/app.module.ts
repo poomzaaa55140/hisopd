4579cd77bd98902d08fcb43eb620995f6030c8ca
Pornsawan Praman <185787855+XcasterZ2@users.noreply.github.com>
2024-12-06 02:18:00 +0700

    test-homev2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { MainContentComponent } from './test-home/main-content/main-content.comp
 import { FirstDivComponent } from './test-home/main-content/first-div/first-div.component';
 import { SecondDivComponent } from './test-home/main-content/second-div/second-div.component';
 import { ThirdDivComponent } from './test-home/main-content/third-div/third-div.component';
+import { ForthDivLeftComponent } from './test-home/main-content/forth-div-left/forth-div-left.component';
+import { ForthDivLeft2Component } from './test-home/main-content/forth-div-left-2/forth-div-left-2.component';
+import { ForthDivLeft3Component } from './test-home/main-content/forth-div-left-3/forth-div-left-3.component';
+import { ForthDivRightComponent } from './test-home/main-content/forth-div-right/forth-div-right.component';
+import { ForthDivRightBtnComponent } from './test-home/main-content/forth-div-right-btn/forth-div-right-btn.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +25,11 @@ import { ThirdDivComponent } from './test-home/main-content/third-div/third-div.
     FirstDivComponent,
     SecondDivComponent,
     ThirdDivComponent,
+    ForthDivLeftComponent,
+    ForthDivLeft2Component,
+    ForthDivLeft3Component,
+    ForthDivRightComponent,
+    ForthDivRightBtnComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test-home/main-content/forth-div-left-2/forth-div-left-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forth-div-left-2',
+  templateUrl: './forth-div-left-2.component.html',
+  styleUrl: './forth-div-left-2.component.scss'
+})
+export class ForthDivLeft2Component {
+
+}
